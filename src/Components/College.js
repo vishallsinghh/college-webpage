18f9229aa4dfbdfd5f0047df5be7c7330e4b500a
vishallsinghh <vishalsingh173911@gmail.com>
2021-06-20 12:42:17 +0530

    First Commit

---
+++ src/Components/College.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const College = () =>{
+    return(
+        <div className='College'>
+            <div className='Prog-offered'>
+                
+            </div>
+        </div>
+    )
+}
+export default College;
